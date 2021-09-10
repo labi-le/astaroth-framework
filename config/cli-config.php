afe39adf5d1e2e78e9e42fd578f7c4dcd2cf138f
labile <1a6i1e@gmail.com>
2021-09-10 06:07:48 +0300

    update examples

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,24 +1,41 @@
 <?php
 
+/**
+ * needed to generate models
+ * you have to change it to fit your needs
+ */
+
+use Astaroth\Auth\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Setup;
 
 require_once "vendor/autoload.php";
 
-//needed to generate models
-//you have to change it to fit your needs
-$config = Setup::createAnnotationMetadataConfiguration(["./App/Entity"], true);
+//Configuration::set(type: \Astaroth\Foundation\Application::PRODUCTION);
+$configuration = Configuration::set(dirname(__DIR__));
+
+//$config = Setup::createAnnotationMetadataConfiguration([getenv("ENTITY_NAMESPACE")]);
+$doctrineConfig = Setup::createAnnotationMetadataConfiguration([$configuration->getEntityNamespace()], true);
+
+//$connection =
+//    [
+//        "driver" => getenv("DATABASE_DRIVER"),
+//        "host" => getenv("DATABASE_HOST"),
+//        "dbname" => getenv("DATABASE_NAME"),
+//        "user" => getenv("DATABASE_USER"),
+//        "password" => getenv("DATABASE_PASSWORD")
+//    ];
 
 $connection =
     [
-        "driver" => "",
-        "host" => "",
-        "dbname" => "",
-        "user" => "",
-        "password" => ""
+        "driver" => $configuration->getDatabaseDriver(),
+        "host" => $configuration->getDatabaseHost(),
+        "dbname" => $configuration->getDatabaseName(),
+        "user" => $configuration->getDatabaseUser(),
+        "password" => $configuration->getDatabasePassword()
     ];
 
-$entityManager = EntityManager::create($connection, $config);
+$entityManager = EntityManager::create($connection, $doctrineConfig);
 
 return ConsoleRunner::createHelperSet($entityManager);
