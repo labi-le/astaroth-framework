c99c5653f812e6254fddb8646ad3d531a1d31b8e
labile <1a6i1e@gmail.com>
2021-11-21 23:41:26 +0300

    упростил примеры

--- app/Command/Example/Attachments.php
+++ app/Command/Example/Attachments.php
@@ -7,33 +7,34 @@ namespace App\Command\Example;
 use Astaroth\Attribute\Conversation;
 use Astaroth\Attribute\Event\MessageNew;
 use Astaroth\Attribute\Message;
+use Astaroth\Commands\BaseCommands;
 use Astaroth\DataFetcher\Events\MessageNew as Data;
-use Astaroth\Support\Facades\Create;
 use Astaroth\Support\Facades\Upload;
 use Astaroth\VkUtils\Builders\Attachments\Photo;
 
 #[Conversation(Conversation::ALL)]
 #[MessageNew]
-class Attachments
+class Attachments extends BaseCommands
 {
     /**
      * @throws \Exception
      * @throws \Throwable
      */
     #[Message("котика")]
-    public function cat(Data $data, Create $create, Upload $upload): void
+    public function cat(Data $data, Upload $upload): void
     {
-        $api = static fn() => json_decode(file_get_contents("https://aws.random.cat/meow"), true)["file"];
+        $api = static fn() => $this->catApi();
 
         $twoCats = [
             new Photo($api()),
             new Photo($api()),
         ];
 
-        $create(
-            (new \Astaroth\VkUtils\Builders\Message())
-            ->setPeerId($data->getPeerId())
-            ->setAttachment(...$upload(...$twoCats))
-        );
+        $this->message($data->getPeerId(), attachment: $upload(...$twoCats));
+    }
+
+    private function catApi(): ?string
+    {
+        return json_decode(file_get_contents("https://aws.random.cat/meow"), true)["file"] ?? null;
     }
 }
