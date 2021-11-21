ad343319ad3cff10ad8f726ea3b48db7f63c9524
labile <1a6i1e@gmail.com>
2021-11-21 23:37:00 +0300

    упростил пример

--- app/Command/Example/Example.php
+++ app/Command/Example/Example.php
@@ -7,20 +7,24 @@ namespace App\Command\Example;
 use Astaroth\Attribute\Conversation;
 use Astaroth\Attribute\Event\MessageNew;
 use Astaroth\Attribute\Message;
+use Astaroth\Commands\BaseCommands;
 use Astaroth\DataFetcher\Events\MessageNew as Data;
 use Astaroth\Support\Facades\Create;
 
 #[Conversation(Conversation::ALL)]
 #[MessageNew]
-class Example
+class Example extends BaseCommands
 {
     #[Message("привет")]
     public function hello(Data $data): void
     {
-        Create::new(
-            (new \Astaroth\VkUtils\Builders\Message())
-                ->setPeerId($data->getPeerId())
-                ->setMessage("Ого! Привет %@name давно не виделись!")
-        );
+        $this->message($data->getPeerId(), "Ого! Привет %@name давно не виделись!");
+
+//        или так
+//        Create::new(
+//            (new \Astaroth\VkUtils\Builders\Message())
+//                ->setPeerId($data->getPeerId())
+//                ->setMessage("Ого! Привет %@name давно не виделись!")
+//        );
     }
 }
