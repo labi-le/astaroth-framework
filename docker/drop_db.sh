9affc82cb8459f6ab9c2348a13824424bc406be5
labile <1a6i1e@gmail.com>
2021-12-09 15:21:03 +0300

    update docker file

--- docker/drop_db.sh
+++ docker/drop_db.sh
@@ -1,1 +1,2 @@
 docker-compose exec app php /app/vendor/bin/doctrine orm:schema-tool:drop --force
+#php vendor/bin/doctrine orm:schema-tool:drop --force
--- docker/run_migrations.sh
+++ docker/run_migrations.sh
@@ -1,1 +1,2 @@
 docker-compose exec app php /app/vendor/bin/doctrine orm:schema-tool:update --force
+#php vendor/bin/doctrine orm:schema-tool:update --force
